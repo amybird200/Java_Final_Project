47d3e926a01ba181b96b876c31ed0e6e616dd0c1
Josie <josie.schmitt@team2502.com>
2019-05-17 10:37:10 -0500

    Set up files

--- Runner.py
+++ Runner.py
@@ -1,5 +1,9 @@
 print "Ask me a question: ",
 question = raw_input()
-print "your question: %r" % (question)
-if question == "hello" :
-    print "Hello to you too",
+print "your question: %r" % question
+
+if question == "hello":
+    print "Hello to you too"
+
+if question == "fuck":
+    print "That's not nice"
