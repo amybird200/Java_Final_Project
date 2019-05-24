9602ffdfa1399a02c5b0ac0c2e43b8ea0fb8e399
Ravisha Jaiswal <Ravisha.jaiswal823@gmail.com>
2019-05-24 08:50:37 -0500

    does this work?

--- Ravisha.py
+++ Ravisha.py
@@ -1,1 +1,20 @@
 #hello my name ravisha and I can't figure out git time to cry please let me code python okay thanks bye goodnight
+
+def smallTalk():
+
+    print "How wassssss your day?"
+    statement = raw_input("> ").lower()
+
+    if "good" in statement:
+        print "Exsssssscelent... What did you do"
+
+
+def nonResponses():
+
+    responses = ["Hmmm", "Tell me more", "Oh?", "Jussst like a bull in a china ssshop...\n...or maybe a ssssnake",
+                 "A chain iss only asss ssstrong asss itsss weakessst link", "Interessssssting...", "Wow", "A fool'ssssss paradisssssse",
+                 "There'ssss a frog in my throat... Yum", "I did not underssstand", "A leopard doesssn't change itssss sspotssss... but a ssssnake sssshedsss itsss ssscalesss",
+                 ]
+    ind = #random num
+
+    print responses[ind]
