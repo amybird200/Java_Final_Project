91e035ac0aedf362e8f3774c3a767ff35a9398e7
nidhikamath <nidhi.kamath@gmail.com>
2019-05-28 09:33:54 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Runner.py

--- Detect.py
+++ Detect.py
@@ -1,7 +1,7 @@
 
 def yesResponses(response):
 
-    yesWords = ["yes", "sure", "ok", "okay", "yeah", "ya", "yah", "yep", "yup", "good", "great", "alright"]
+    yesWords = ["yes", "sure", "ok", "okay", "yeah", "ya", "yah", "yep", "yup", "good", "great", "alright", "yee"]
     for i in range(0, len(yesWords)):
         if yesWords[i] in response.lower():
             return True
